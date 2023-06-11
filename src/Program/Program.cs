a5e2af35e1eea10e5c1477459221f094e85f9cae
Cristianbaumann <Cristian.baumann@correo.ucu.edu.uy>
2023-06-11 14:09:37 -0300

    JOya

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
 using Library;
 
 namespace Program
@@ -7,13 +9,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Node n1 = new Node(new Persona("Cristian",19));
+            Node n2 = new Node(new Persona("Jorge",15));
+            Node n3 = new Node(new Persona("Marcos",48));
+            Node n4 = new Node(new Persona("Nicolas",17));
+            Node n5 = new Node(new Persona("Juan",65));
+            Node n6 = new Node(new Persona("Santiago",85));
+            Node n7 = new Node(new Persona("Franceso",76));
+            Node n8 = new Node(new Persona("Sebastian",12));
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,6 +26,7 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            n2.AddChildren(n8);
 
             // visitar el árbol aquí
         }
